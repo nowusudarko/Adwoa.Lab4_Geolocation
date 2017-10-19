daf4478b7ed938ced45eb8b40fb6ecb6ffdd6332
Nana Adwoa Owusu Darko <nowusudarko@gmail.com>
2017-10-19 18:55:36 +0000

    final commit

--- js/app/app.js
+++ js/app/app.js
@@ -12,26 +12,12 @@ function onDeviceReady() {
 }
 
 // onSuccess Geolocation
-//
+//modified function to show map and marker
 function onSuccess(position) {
-
-    // var element = document.getElementById('geolocation');
-    // element.innerHTML = 'Latitude: ' + position.coords.latitude  + '<br />' +
-    //     'Longitude: '          + position.coords.longitude             + '<br />' +
-    //     'Altitude: '           + position.coords.altitude              + '<br />' +
-    //     'Accuracy: '           + position.coords.accuracy              + '<br />' +
-    //     'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-    //     'Heading: '            + position.coords.heading               + '<br />' +
-    //     'Speed: '              + position.coords.speed                 + '<br />' +
-    //     'Timestamp: '          + position.timestamp          + '<br />';
-
-
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var position = new google.maps.LatLng(lat, long);
     var map = document.getElementById('map');
-
-
 
     var mapProperties = {
         center:position,zoom:20,
